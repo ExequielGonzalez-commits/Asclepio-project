8b5540d374e138adafd6780f237ffdffc3aa8592
ExequielGonzalez-commits <exequielgonzalez395@gmail.com>
2025-10-11 16:14:33 -0300

    agregando token de usuario

--- frontendPage/firebase-messaging-sw.js
+++ frontendPage/firebase-messaging-sw.js
@@ -4,10 +4,13 @@
   //import{onBackgroundMessage} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-messaging.js"
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
-  importScripts('https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/12.4.0/firebase-messaging.js');
+  //importScripts('https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js');
+//importScripts('https://www.gstatic.com/firebasejs/12.4.0/firebase-messaging.js');
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  importScripts('https://www.gstatic.com/firebasejs/9.6.11/firebase-app-compat.js');
+  importScripts('https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging-compat.js');
+
   const firebaseConfig = {
     apiKey: "AIzaSyAZL9kxHHa-CniQ6LJndV85XyApc_jzM8I",
     authDomain: "asclepio-project.firebaseapp.com",
