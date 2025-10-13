919eef0492c355e5f57fc1481dce2c4ad0b1e7bd
ExequielGonzalez-commits <exequielgonzalez395@gmail.com>
2025-10-12 21:20:22 -0300

    feature de eliminar duplicados

--- app.py
+++ app.py
@@ -143,5 +143,6 @@ def home():
 if __name__ == "__main__":    
     with app.app_context():
         db.create_all()
+        eliminar_duplicados()
     
     app.run(debug=True,port=5000,host="0.0.0.0")
