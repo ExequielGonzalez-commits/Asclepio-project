1832423c33ecb8e4334e58799d911255eb3cb892
Exequiel-Gonzalez-commits <exequielgonzalez395@gmail.com>
2025-11-13 16:06:15 -0300

    acutalizar

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ if __name__ == "__main__":
         db.create_all()
         eliminar_duplicados()
     
-    app.run(debug=True,port=1000,host="0.0.0.0")
+    app.run(debug=True,port=5000,host="0.0.0.0")
